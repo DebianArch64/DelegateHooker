d90895d4e864d47750c2cd7daa865cd7b6780fee
DebianArch <63203414+DebianArch64@users.noreply.github.com>
2022-12-10 22:33:33 -0600

    Fix appdelegate not hooking

--- DelegateHooker.h
+++ DelegateHooker.h
@@ -9,13 +9,13 @@
 #import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface HookDelegate : UIResponder <UIApplicationDelegate>
 
 @property (nonatomic) NSMutableArray<void (^)(id)> *instructions;
 @property (nonatomic) BOOL isDelegateSupported;
 @property (nonatomic) BOOL didDelegateLoad;
 
-+(AppDelegate *_Nonnull)shared;
++(HookDelegate *_Nonnull)shared;
 -(BOOL)redirectDelegate:(SEL)selector delegate:(id)hook error:(NSError**)error asyncError:(void (^)(NSError*error))errorHandler;
 -(BOOL)redirectDelegate:(SEL)selector originalDelegate:(id)originalDelegate delegate:(id)hook skipHandler:(BOOL)skipHandler error:(NSError**)error asyncError:(void (^)(NSError*error))errorHandler;
 @end
